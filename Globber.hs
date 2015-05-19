7708d556b6d259bbc271eae50d91665223470b27
Lee Ehudin <percivalgambit@gmail.com>
2015-05-18 22:37:18 -0500

    refactoring

--- Globber.hs
+++ Globber.hs
@@ -5,20 +5,20 @@ import Data.List
 type GlobPattern = String
 
 matchGlob :: GlobPattern -> String -> Bool
-matchGlob glob@(globFirst:globRest) matched = case matched of
-    "" | glob == "*" -> True
-       | otherwise   -> False
-    (matchedFirst:matchedRest) -> case globFirst of
-        '\\'
-            | globRest == ""                -> False
-            | head globRest == matchedFirst -> matchGlob (tail globRest) matchedRest
-            | otherwise                     -> False
-        '*'
-            | globRest == "" -> True
-            | otherwise      -> or $ fmap (matchGlob globRest) (tails matched)
-        '?' -> matchGlob globRest matchedRest
-        _
-            | globFirst == matchedFirst -> matchGlob globRest matchedRest
-            | otherwise                 -> False
-matchGlob "" "" = True
-matchGlob "" _  = False
+matchGlob (globFirst:globRest) matched@(matchedFirst:matchedRest) = case globFirst of
+    '\\' -> case globRest of
+        ""  -> False
+        (literalChar:rest)
+            | literalChar == matchedFirst -> matchGlob rest matchedRest
+            | otherwise                   -> False
+    '*' -> case globRest of
+        "" -> True
+        _  -> or $ fmap (matchGlob globRest) (tails matched)
+    '?' -> matchGlob globRest matchedRest
+    _
+        | globFirst == matchedFirst -> matchGlob globRest matchedRest
+        | otherwise                 -> False
+matchGlob ""  "" = True
+matchGlob "*" "" = True
+matchGlob _   "" = False
+matchGlob ""  _  = False
